35c6b1733b20051171aaccfafdc82d481b2b85a9
VikasKumarRoy <vikas.vr8@gmail.com>
2019-08-08 13:01:55 +0530

    Distribute Candies

---
+++ Problem-1103.cpp
@@ -0,0 +1,23 @@
+//Problem  - 1103
+// https://leetcode.com/problems/distribute-candies-to-people/
+
+
+class Solution {
+public:
+    vector<int> distributeCandies(int candies, int num_people) {
+        vector <int> ans(num_people);
+        if(candies == 0)
+            return ans;
+        int ctr = 1;
+        int i = 0;
+        while(ctr <= candies) {
+            ans[i++] += ctr;
+            candies -= ctr;
+            ctr++;
+            if(i == ans.size())
+                i = 0;
+        }
+        ans[i] += candies;
+        return ans;
+    }
+};
