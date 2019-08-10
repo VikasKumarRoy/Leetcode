1d5e6b88426ac38f0d1406c76eec37352d20d83d
VikasKumarRoy <vikas.vr8@gmail.com>
2019-08-10 23:50:39 +0530

    Merge K soted Linked List

---
+++ Problem-23.cpp
@@ -0,0 +1,30 @@
+//Problem  - 23
+// https://leetcode.com/problems/merge-k-sorted-lists/
+
+
+// O(nlogk) solution passes all tc
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size() < 1)
+            return NULL;
+        priority_queue <pair <int, ListNode*>, vector <pair <int, ListNode*>>, greater<pair <int, ListNode*>> > pq;
+        for(int i = 0; i < lists.size(); i++) {
+            if(lists[i])
+                pq.push({lists[i]->val, lists[i]});
+        }
+        ListNode *head = new ListNode(21);
+        ListNode *curr = head;
+        while(!pq.empty()) {
+            auto top = pq.top();
+            pq.pop();
+            curr->next = top.second;
+            if(top.second->next)
+                pq.push({top.second->next->val, top.second->next});
+            curr = curr->next;
+        }
+        curr->next = NULL;
+        return head->next;   
+    }
+};
