6be934c32a2b0f9f08997b7a3b13dfa674f0c195
VikasKumarRoy <vikas.vr8@gmail.com>
2019-08-19 11:50:36 +0530

    Jump Game II

---
+++ Problem-45.cpp
@@ -0,0 +1,22 @@
+//Problem  - 45
+// https://leetcode.com/problems/jump-game-ii/
+
+// O(n) greedy solution usign bfs
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        if(nums.size() == 0 || nums[0] == 0)
+            return false;
+        
+        int farthest = 0;
+        int end = 0, jumps = 0;
+        for(int i = 0; i < nums.size()-1; i++) {
+            farthest = max(farthest, i + nums[i]);
+            if(i == end) {
+                jumps++;
+                end = farthest;
+            }
+        }
+        return jumps;
+    }
+};
