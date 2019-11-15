6a360a7e3c43ee3c303f43c6b110a9697b380a9b
AbhishekKumarRoy <vikas.vr8@gmail.com>
2019-11-15 23:54:03 +0530

    Stone Game(Medium)

---
+++ Problem-877.cpp
@@ -0,0 +1,12 @@
+//Problem - 877
+
+// https://leetcode.com/problems/stone-game/
+
+// O(1)  time complexity and O(1) space complexity sol
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
