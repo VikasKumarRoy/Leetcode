eef6fb94c4b0d55638c59d303ab1f3c9a2d6abc4
AbhishekKumarRoy <vikas.vr8@gmail.com>
2019-11-18 23:52:22 +0530

    Shift 2D Grid(Easy)

---
+++ Problem-1260.cpp
@@ -0,0 +1,14 @@
+//Problem - 1260
+
+// https://leetcode.com/problems/shift-2d-grid/	
+
+// O(n*m) time complexity and O(1) space complexity sol
+
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+    }
+};
