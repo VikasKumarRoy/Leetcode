dd7baac3aac0dd46a1aac4a8232f11bbb8338b6e
AbhishekKumarRoy <vikas.vr8@gmail.com>
2019-11-22 23:59:43 +0530

    Hamming Distance(Easy)

---
+++ Problem-461.cpp
@@ -0,0 +1,19 @@
+//Problem - 461
+
+// https://leetcode.com/problems/hamming-distance/
+
+// O(n) time complexity and O(1) space complexity sol
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int count = 0;
+        while(x || y) {
+            if(x & 1 != y & 1)
+                count++;
+            x = x >> 1;
+            y = y >> 1;
+        }
+        return count;
+    }
+};
