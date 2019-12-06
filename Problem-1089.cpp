0fe9c22eeb0f80eb210681cecaf2f2acc202a528
AbhishekKumarRoy <vikas.vr8@gmail.com>
2019-12-06 23:57:46 +0530

    Duplicate Zeros(Easy)

---
+++ Problem-1089.cpp
@@ -0,0 +1,12 @@
+//Problem - 1089
+
+// https://leetcode.com/problems/duplicate-zeros/
+
+// O(n) time complexity and O(1) space complexity solution
+
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        
+    }
+};
