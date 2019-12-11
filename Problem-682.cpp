43e060bd5b02b23d7cab836d19a812b738740294
AbhishekKumarRoy <vikas.vr8@gmail.com>
2019-12-11 22:53:41 +0530

    Baseball Game(Easy)

--- Problem-682.cpp
+++ Problem-682.cpp
@@ -2,9 +2,36 @@
 
 // https://leetcode.com/problems/baseball-game/
 
-// O(n) time complexity and O(1) space complexity solution
+// O(n) time complexity and O(n) space complexity solution
 
 class Solution {
 public:
-    v
+    
+    int calPoints(vector<string>& ops) {
+        int ans = 0;
+        stack <int> s;
+        for(int i = 0; i < ops.size(); i++) {
+            if(ops[i] == "C") {
+                ans -= s.top();
+                s.pop();
+            }
+            else if(ops[i] == "D") {
+                ans += s.top() * 2;
+                s.push(s.top()*2);
+            }
+            else if(ops[i] == "+") {
+                int t = s.top();
+                s.pop();
+                int val = s.top() + t;
+                ans += val;
+                s.push(t);
+                s.push(val);
+            }
+            else {
+                ans += stoi(ops[i]);
+                s.push(stoi(ops[i]));
+            }
+        }
+        return ans;
+    }
 };
