278b12f0cd989bbfb970e959bf94a392ffc4db47
AbhishekKumarRoy <vikas.vr8@gmail.com>
2019-12-25 23:56:05 +0530

    Last Stone Weight(Easy)

---
+++ Problem-1046.cpp
@@ -0,0 +1,21 @@
+// Problem - 1046
+
+// https://leetcode.com/problems/last-stone-weight/
+
+// O(nlogn) time complexity and O(n) space complexity solution
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue <int> pq(stones.begin(), stones.end());
+        while(pq.size() > 1) {
+            int a = pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+            if(a != b)
+                pq.push(abs(a-b));
+        }
+        return pq.empty() ? 0 : pq.top();
+    }
+};
