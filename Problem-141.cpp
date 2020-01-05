2aa5e919139bc0ef75bca0df65b85d0a6ca80fa9
AbhishekKumarRoy <vikas.vr8@gmail.com>
2020-01-06 00:06:40 +0530

    Linked List Cycle(Easy)

--- Problem-141.cpp
+++ Problem-141.cpp
@@ -8,7 +8,7 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
         ListNode *slow = head, *fast = head;
-        while(slow != NULL && fast->next != NULL) {
+        while(fast && fast->next) {
             slow = slow->next;
             fast = fast->next->next;
             if(slow == fast)
