2af3610579ee2eec56460a6de0b17aca00e73396
AbhishekKumarRoy <vikas.vr8@gmail.com>
2020-01-12 23:59:22 +0530

    Convert Integer to the Sum of Two No-Zero Integers(Easy)

---
+++ Problem-1317.cpp
@@ -0,0 +1,20 @@
+// Problem - 1317
+
+// https://leetcode.com/problems/convert-integer-to-the-sum-of-two-no-zero-integers/
+
+// O(nlog10n) time complexity and O(1) space complexity solution
+
+class Solution {
+public:
+    vector<int> getNoZeroIntegers(int n) {
+        string s1, s2;
+        for(int i = 1; i < n; ++i) {
+            int j = n - i;
+            s1 = to_string(i);
+            s2 = to_string(j);
+            if (s1.find('0') == s1.end() && s2.find('0') == s2.end())
+                return {i, j};
+        }
+    return {};
+}
+};
