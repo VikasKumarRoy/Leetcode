23d20b5ce17dd8c92a8d2e84c7a4487923d985a4
AbhishekKumarRoy <vikas.vr8@gmail.com>
2020-01-18 00:55:47 +0530

    Isomorphic Strings(Easy)

--- Problem-205.cpp
+++ Problem-205.cpp
@@ -2,17 +2,22 @@
 
 // https://leetcode.com/problems/isomorphic-strings/
 
-// O(n) time complexity and O(n) space complexity solution
+// O(n) time complexity and O(1) space complexity solution
 
 class Solution {
 public:
-    bool checkPossibility(vector<int>& nums) {
-        bool flag = 0;
-        for(int i = 1; i < nums.size(); i++) {
-            if(nums[i] < nums[i-1]) {
-                if(flag)
+    bool isIsomorphic(string s, string t) {
+        unordered_set <char> us;
+        unordered_map <char, char> um;
+        for(int i = 0; i < s.length(); i++) {
+            if(um.find(s[i]) == um.end()) {
+                if(us.count(t[i]))
                     return 0;
-                flag = 1;
+                um[s[i]] = t[i];
+                us.insert(t[i]);
+            } else {
+                if(um[s[i]] != t[i])
+                    return 0;
             }
         }
         return 1;
