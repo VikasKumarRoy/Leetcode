5db8c452e68e7c420aac8d9fe10228836825c568
AbhishekKumarRoy <vikas.vr8@gmail.com>
2020-01-27 23:58:58 +0530

    Keyboard Row(Easy)

---
+++ Problem-500.cpp
@@ -0,0 +1,27 @@
+// Problem - 500
+
+// https://leetcode.com/problems/keyboard-row/
+
+// O(n) time complexity and O(1) space complexity solution
+
+class Solution {
+public:
+    
+    int map [26]= {2,3,3,2,1,2,2,2,1,2,2,2,3,3,1,1,1,1,2,1,1,3,1,3,1,3};
+
+    vector<string> findWords(vector<string>& words) {
+        vector <string> ans;
+        for(int i = 0; i < words.size(); i++) {
+            int m = map[words[i][0] - 'a'];
+            int flag = 1;
+            for(int j = 1; j < words[i].size(); j++)
+                if(map[words[i][j] - 'a'] != m) {
+                    flag = 0;
+                    break;
+                }
+            if(flag)
+                ans.push_back(words[i]);
+        }
+        return ans;
+    }
+};
