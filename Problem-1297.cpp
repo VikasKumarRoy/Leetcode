10ea8dc503a32ad2b5ac10b14fd762b42bd4102c
AbhishekKumarRoy <vikas.vr8@gmail.com>
2020-01-31 18:26:44 +0530

    Maximum Number of Occurrences of a Substring(Medium)

--- Problem-1297.cpp
+++ Problem-1297.cpp
@@ -2,11 +2,33 @@
 
 // https://leetcode.com/problems/maximum-number-of-occurrences-of-a-substring/
 
-// O(n) time complexity and O(n) space complexity solution
+// O(n) time complexity and O(n) space complexity solution using sliding window
 
 class Solution {
 public:
     int maxFreq(string s, int maxLetters, int minSize, int maxSize) {
-        
+        int ans = 0;
+        unordered_map <string, int> um;
+        int freq[26] = {0};
+        int i = 0, j = 0, count = 0;
+        while(j < s.length()) {
+            if(freq[s[j]-'a'] == 0)
+                    count++;
+            freq[s[j]-'a']++;
+            if((j - i + 1) > minSize) {
+                cout << "l;";
+                if(--freq[s[i]-'a'] == 0)
+                    count--;
+                i++;
+            }
+            if((j - i  + 1) >= minSize && count <= maxLetters)
+                um[s.substr(i, j-i+1)]++;
+            j++;
+        }
+        for(auto p : um) {
+            cout << p.first<<endl;
+            ans = max(ans, p.second);
+        }
+        return ans;
     }
 };
