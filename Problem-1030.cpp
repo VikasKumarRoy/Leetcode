ac064442c22a68282187ff733ede7bb2b512d6a0
AbhishekKumarRoy <vikas.vr8@gmail.com>
2020-02-01 23:59:11 +0530

    Matrix Cells in Distance Order(Easy)

---
+++ Problem-1030.cpp
@@ -0,0 +1,13 @@
+// Problem - 1030
+
+// https://leetcode.com/problems/matrix-cells-in-distance-order/
+
+// O(n*m) time complexity and O(n) space complexity solution using sliding window
+
+class Solution {
+public:
+    vector<vector<int>> allCellsDistOrder(int R, int C, int r0, int c0) {
+        vector <vector <int>> ans(R, vector <int> (C, 0));
+        return ans;
+    }
+};
