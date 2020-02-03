a9cd75a2b9aac1deea8095d3c84380c760348d1f
AbhishekKumarRoy <vikas.vr8@gmail.com>
2020-02-03 22:53:41 +0530

    The K Weakest Rows in a Matrix(Easy)

---
+++ Problem-1341.cpp
@@ -0,0 +1,12 @@
+// Problem - 1341
+
+// https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/
+
+// O(n*m) time complexity and O(n*m) space complexity solution using bfs
+
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        
+    }
+};
