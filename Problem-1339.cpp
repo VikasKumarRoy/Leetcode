bf5c44d5e5e43710627c4ee036055950c54f19d0
AbhishekKumarRoy <vikas.vr8@gmail.com>
2020-02-09 02:01:34 +0530

    Maximum Product of Splitted Binary Tree(Medium)

--- Problem-1339.cpp
+++ Problem-1339.cpp
@@ -2,30 +2,30 @@
 
 // https://leetcode.com/problems/maximum-product-of-splitted-binary-tree/
 
-// O(n) time complexity and O(n) space complexity solution
+// O(n) time complexity and O(height) space complexity solution
 
 class Solution {
 public:
     int mod = 1e9 + 7;
     
-    int dfsSum(TreeNode* root) {
+    int getSum(TreeNode* root) {
         if(!root) {
             return 0;
         }
-        return root->val + dfsSum(root->left) + dfsSum(root->right);
+        return root->val + getSum(root->left) + getSum(root->right);
     }
     
-    int dfs(TreeNode* root, int& mx, int sum) {
+    long long dfs(TreeNode* root, long long& mx, int sum) {
         if(!root) {
             return 0;
         }
-        int curr = root->val + dfs(root->left, mx, sum) + dfs(root->right, mx, sum);
-        mx = max(mx, ((curr%mod)*(sum-curr)%mod)%mod);
+        long curr = root->val + dfs(root->left, mx, sum) + dfs(root->right, mx, sum);
+        mx = max(mx, ;
         return curr;
     }
     
     int maxProduct(TreeNode* root) {
-        int sum = dfsSum(root);
+        int sum = getSum(root);
         int mx = 0;
         dfs(root, mx, sum);
         return mx;
