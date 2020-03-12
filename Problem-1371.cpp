5eed2479c6176bb28204cdf8c81f308a0079589d
Vikas Roy <vikas.vr8@gmail.com>
2020-03-12 22:48:33 +0530

    Find the Longest Substring Containing Vowels in Even Counts(Medium)

--- Problem-1371.cpp
+++ Problem-1371.cpp
@@ -2,6 +2,34 @@
 
 // https://leetcode.com/problems/find-the-longest-substring-containing-vowels-in-even-counts/
 
-// O(n) time complexity and O(1) space complexity solution using bit mask
+// O(n) time complexity and O(n) space complexity solution
 
-
+class Solution {
+public:
+    string sortString(string s) {
+        int n = s.length();
+        string res = "";
+        map <char, int> m;
+        for(char c : s)
+            m[c]++;
+        while(1) {
+            for(auto it = m.begin(); it != m.end(); it++) {
+                if(it->second > 0) {
+                    res += it->first;
+                    it->second--;
+                }
+                if(res.length() == n)
+                    return res;
+            }
+            for(auto it = m.rbegin(); it != m.rend(); it++) {
+                if(it->second > 0) {
+                    res += it->first;
+                    it->second--;
+                }
+                if(res.length() == n)
+                    return res;
+            }
+        }
+        return res;
+    }
+};
