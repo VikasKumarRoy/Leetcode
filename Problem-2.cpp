bd4238c63314b1544ff54fc2d01dea976256ecf5
VikasKumarRoy <vikas.vr8@gmail.com>
2020-03-25 01:21:27 +0530

    Add Two Numbers(Medium)

--- Problem-2.cpp
+++ Problem-2.cpp
@@ -9,39 +9,42 @@ public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int carry = 0;
         ListNode* head1 = l1, *head2 = l2;
-        ListNode newHead = ListNode(0);
-        ListNode *curr = &newHead;
+        ListNode* newHead = new ListNode(0);
+        ListNode* curr = newHead;
         while(head1 && head2) {
             int sum = carry + head1->val + head2->val;
             carry = sum / 10;
             sum = sum % 10;
-            ListNode temp = ListNode(sum);
-            curr->next = &temp;
+            ListNode* temp = new ListNode(sum);
+            curr->next = temp;
             head1 = head1->next;
             head2 = head2->next;
             curr = curr->next;
         }
-        if(head1) {
+        while(head1) {
             int sum = carry + head1->val;
             carry = sum / 10;
             sum = sum % 10;
-            curr->val = sum;
-            ListNode temp = ListNode(sum);
-            curr->next = &temp;
+            ListNode* temp = new ListNode(sum);
+            curr->next = temp;
             head1 = head1->next;
             curr = curr->next;
         }
-        if(head2) {
+        while(head2) {
             int sum = carry + head2->val;
             carry = sum / 10;
             sum = sum % 10;
-            ListNode temp = ListNode(sum);
-            curr->next = &temp;
+            ListNode* temp = new ListNode(sum);
+            curr->next = temp;
             head2 = head2->next;
             curr = curr->next;
         }
+        if(carry) {
+           ListNode *temp = new ListNode(carry);
+           curr->next = temp;
+           curr = curr->next;
+        }
         curr->next = NULL;
-        ListNode* head = &newHead;
-        return head->next;
+        return newHead->next;
     }
 };
