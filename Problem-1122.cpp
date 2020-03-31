4e4318fdda8ae34bdc01cf1581dc99c771253078
Vikas Roy <vikas.vr8@gmail.com>
2020-03-31 23:59:14 +0530

    Relative Sort Array(Easy)

---
+++ Problem-1122.cpp
@@ -0,0 +1,16 @@
+//Problem  - 1122
+
+// https://leetcode.com/problems/relative-sort-array/
+
+// O(nlogn) time complexity and O(1) space solution 
+
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        set <int> s;
+        vector <int> ans;
+        unordered_map <int, int> um(arr2.begin(), arr2.end());
+        
+        
+    }
+};
