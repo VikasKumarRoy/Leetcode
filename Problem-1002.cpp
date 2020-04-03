e1d6568298eb61d3719f8425e704585b8af2a64f
Vikas Roy <vikas.vr8@gmail.com>
2020-04-03 23:53:41 +0530

    Find Common Characters(Easy)

--- Problem-1002.cpp
+++ Problem-1002.cpp
@@ -2,11 +2,30 @@
 
 // https://leetcode.com/problems/find-common-characters/
 
-// O(n*m) time complexity and O(n*m) space complexity solution using bfs
+// O(n) time complexity and O(1) space complexity solution where n = no. of chars
 
 class Solution {
 public:
     vector<string> commonChars(vector<string>& A) {
-        
+        vector <int> common(26, 0);
+        vector <string> ans;
+        for(char c : A[0]) {
+            common[c-'a']++;
+        }
+        for(int i = 1; i < A.size(); i++) {
+            vector <int> freq(26, 0);
+            for(char c : A[i]) {
+                freq[c-'a']++;
+            }
+            for(int j = 0; j < 26; j++) {
+                common[j] = min(common[j], freq[j]);
+            }
+        }
+        for(int i = 0; i < 26; i++) {
+            while(common[i]--)
+                ans.push_back(string(1, i+'a'));
+        }
+        return ans;
     }
 };
+
