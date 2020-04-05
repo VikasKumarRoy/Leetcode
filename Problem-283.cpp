c5ab80bc42ab4d6483f849c978e891f0ee8a78b3
Vikas Roy <vikas.vr8@gmail.com>
2020-04-06 01:22:43 +0530

    Move Zeroes(Easy)

--- Problem-283.cpp
+++ Problem-283.cpp
@@ -2,23 +2,16 @@
 
 // https://leetcode.com/problems/move-zeroes/
 
-// O(n) time complexity and O(1) space complexity solution where n = no. of chars
+// O(n) time complexity and O(1) space complexity solution
 
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i = 0;
-        while(i < nums.size()) {
-            if(nums[i] == 0) {
-                int j = i+1;
-                while(j < nums.size() && nums[j] == 0) {
-                    j++;
-                }
-                if(j < nums.size()) {
-                    swap(nums[i], nums[j]);
-                }
-            }
-            i++;
+        int lastPos = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] != 0)
+                swap(nums[lastPos++], nums[i]);
         }
+        return;
     }
 };
