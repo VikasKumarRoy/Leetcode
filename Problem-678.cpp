70cf88757416cbe0fdec804824686be5ad9471c9
Vikas Roy <vikas.vr8@gmail.com>
2020-04-17 02:08:36 +0530

    Valid Parenthesis String(Medium)

--- Problem-678.cpp
+++ Problem-678.cpp
@@ -2,11 +2,23 @@
 
 // https://leetcode.com/problems/valid-parenthesis-string/
 
-// O(n) time complexity and O(n) space complexity solution
+// O(n) time complexity and O(1) space complexity solution
 
 class Solution {
 public:
     bool checkValidString(string s) {
-        
+        int mn = 0, mx = 0;
+        for(char c : s) {
+            if(c == '(')
+                mn++, mx++;
+            else if(c == '*')
+                mx++, mn = max(0, --mn);
+            else {
+                mx--; mn = max(0, --mn);
+            }
+            if(mx < 0)
+                return 0;
+        }
+        return mn == 0;
     }
 };
