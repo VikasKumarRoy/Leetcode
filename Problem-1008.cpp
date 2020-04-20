a329385e969dcaed0824e54de75f611a110423f8
Vikas Roy <vikas.vr8@gmail.com>
2020-04-20 23:58:39 +0530

    Construct Binary Search Tree from Preorder Traversal(Medium)

---
+++ Problem-1008.cpp
@@ -0,0 +1,20 @@
+// Problem - 1008
+
+// https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
+
+// O(n) time complexity and O(n) space complexity solution
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        
+    }
+};
