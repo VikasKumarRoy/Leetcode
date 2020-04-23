9a936006960e5b5412a9cef59296382d04658125
Vikas Roy <vikas.vr8@gmail.com>
2020-04-24 01:25:36 +0530

    Bitwise AND of Numbers Range(Medium)

--- Problem-201.cpp
+++ Problem-201.cpp
@@ -2,11 +2,17 @@
 
 // https://leetcode.com/problems/bitwise-and-of-numbers-range/
 
-// O(n) time complexity and O(n) space complexity solution
+// O(n) time complexity and O(1) space complexity solution
 
 class Solution {
 public:
     int rangeBitwiseAnd(int m, int n) {
-        
+        int shift = 0;
+        while(m != n) {
+            m >>= 1;
+            n >>= 1;
+            shift++;
+        }
+        return m << shift;
     }
 };
