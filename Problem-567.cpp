b208f0e78b6721a3e5f66da2aba2b80870e0ba64
Vikas Roy <vikas.vr8@gmail.com>
2020-05-19 02:31:15 +0530

    Permutation in String(Medium)

--- Problem-567.cpp
+++ Problem-567.cpp
@@ -1,13 +1,36 @@
- 
-// Problem - 1030
+// Problem - 567
 
 // https://leetcode.com/problems/permutation-in-string/
 
-// O(n) time complexity and O(n) space complexity solution
+// O(n+m) time complexity and O(m) space complexity solution using sliding window
 
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        
+        vector <int> freq(26, 0);
+        for(char c : s1)
+            freq[c-'a']++;
+        int i = 0, j =0;
+        int len = 0, n = s1.length();
+        while(j < s2.length()) {
+            if(freq[s2[j]-'a']) {
+                len++;
+                freq[s2[j]-'a']--;
+                j++;
+            }
+            else {
+                if(i == j) {
+                    i++, j++;
+                }
+                else {
+                    freq[s2[i]-'a']++;
+                    i++;
+                    len--;
+                }
+            }
+            if(len == n)
+                return true;
+        }
+        return false;
     }
 };
