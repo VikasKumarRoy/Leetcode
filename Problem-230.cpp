2171f8fdd341c0ef986220edcc02c098c62e27e5
Vikas Roy <vikas.vr8@gmail.com>
2020-05-22 01:55:43 +0530

    Kth Smallest Element in a BST(Medium)

--- Problem-230.cpp
+++ Problem-230.cpp
@@ -1,43 +1,53 @@
- 
 // Problem - 230
 
 // https://leetcode.com/problems/kth-smallest-element-in-a-bst/
 
-// O(n) time complexity for construction and O(h) time for serching
-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// O(n) time complexity for construction and O(h) time for searching
 
 struct Node {
     int val;
-    Node *left, right;
-    int count;
+    Node *left, *right;
+    int leftCount, totalCount;
     Node(int x) {
         val = x;
         left = right = NULL;
-        count = 1;
+        leftCount = totalCount = 1;
     }
 };
 
 class Solution {
 public:
-    Node* buildMyTree(root) {
+    Node* buildMyTree(TreeNode *root) {
         if(!root)
             return NULL;
-        
+        Node *newNode = new Node(root->val);
+        Node *lv = buildMyTree(root->left);
+        Node *rv = buildMyTree(root->right);
+        if(lv) {
+            newNode->leftCount += lv->totalCount;
+            newNode->totalCount += lv->totalCount;
+        }
+        if(rv)
+            newNode->totalCount += rv->totalCount;
+        newNode->left = lv;
+        newNode->right = rv;
+        return newNode;
     }
     
+    int kthSmallestUtil(Node *root, int k) {
+        if(k < 1 || k > root->totalCount)
+            return -1;
+        if(k == root->leftCount)
+            return root->val;
+        else if(k < root->leftCount) {
+            return kthSmallestUtil(root->left, k);
+        }
+        else
+            return kthSmallestUtil(root->right, k - root->leftCount);
+    }
     
     int kthSmallest(TreeNode* root, int k) {
-        buildMyTree(root);
+        Node *newRoot = buildMyTree(root);
+        return kthSmallestUtil(newRoot, k);
     }
 };
