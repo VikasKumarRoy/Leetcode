99310f5a9af7a5f59d7e6227539818ef52011fd2
Vikas Roy <vikas.vr8@gmail.com>
2020-05-26 02:29:13 +0530

    Uncrossed Lines(Medium)

--- Problem-1035.cpp
+++ Problem-1035.cpp
@@ -2,11 +2,22 @@
 
 // https://leetcode.com/problems/uncrossed-lines/
 
-// O(n) time complexity and O(1) space complexity solution
+// O(n*m) time complexity and O(n*m) space complexity solution using dp similar to longest common subsequence
 
 class Solution {
 public:
     int maxUncrossedLines(vector<int>& A, vector<int>& B) {
-        
+        int n = A.size(), m = B.size();
+        vector <vector<int>> dp(n+1, vector <int> (m+1, 0));
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(A[i-1] == B[j-1]) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
     }
 };
