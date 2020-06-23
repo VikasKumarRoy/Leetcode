4cc07b6d83625a1cf2d557e26bf53d646113921d
Vikas Roy <vikas.vr8@gmail.com>
2020-06-24 02:00:35 +0530

    Count Complete Tree Nodes(Medium)

--- Problem-222.cpp
+++ Problem-222.cpp
@@ -1,4 +1,5 @@
-//Problem - 222
+// Problem - 222
+
 // https://leetcode.com/problems/count-complete-tree-nodes/
 
 // Time Complexity O(n) and O(1) space ignoring stack space
@@ -11,3 +12,30 @@ public:
     }
 };
 
+//          OR
+
+// O(logn*logn) time complexity O(1) space 
+
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(!root)
+            return 0;
+        TreeNode *l, *r;
+        l = r = root;
+        int lh = 0, rh = 0;
+        while(l) {
+            lh++;
+            l = l->left;
+        }
+        while(r) {
+            rh++;
+            r = r->right;
+        }
+        if(lh == rh) {
+            //return pow(2, lh) - 1;       less efficient
+            return (1 << lh) - 1;
+        }
+        return countNodes(root->left) + countNodes(root->right) + 1;
+    }
+};
