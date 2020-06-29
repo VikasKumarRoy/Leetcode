b546a13a6d81cf80eda9a2fd253a310f87afb4f3
Vikas Roy <vikas.vr8@gmail.com>
2020-06-29 23:59:50 +0530

    Path Crossing(Easy)

--- Problem-1496.cpp
+++ Problem-1496.cpp
@@ -2,14 +2,33 @@
 
 // https://leetcode.com/problems/path-crossing/
 
-// O(n) time complexity and O(1) space complexity solution
+// O(n) time complexity and O(n) space complexity solution
 
 class Solution {
 public:
     bool isPathCrossing(string path) {
-        
+        unordered_set < pair<int, int>, bool, hash_pair > us;
+        pair <int, int> p = {0, 0};
+        us.insert(p);
+        for(char c : path) {
+            switch(c) {
+                case 'N':
+                    p.first++;
+                    break;
+                case 'S':
+                    p.first--;
+                    break;
+                case 'W':
+                    p.second--;
+                    break;
+                case 'E':
+                    p.second++;
+                    break;
+            }
+            if(us.count(p))
+                return true;
+            us.insert(p);
+        }
+        return false;
     }
 };
-
-
-
