bc50b97328a97383dc7fba4902b8a18822bfe4fd
Vikas Roy <vikas.vr8@gmail.com>
2020-07-06 01:45:55 +0530

    Can Make Arithmetic Progression From Sequence(Easy)

--- Problem-1502.cpp
+++ Problem-1502.cpp
@@ -7,6 +7,37 @@
 class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
+        int n = arr.size();
+        if (n < 3)
+            return true;
+        int mx = *max_element(arr.begin(), arr.end());
+        int mn = *min_element(arr.begin(), arr.end());
+        if((mx-mn)%(n-1) != 0)
+            return false;
+        int d = (mx-mn)/(n-1);
+        int i = 0;
+        while(i < arr.size()) {
+            if(arr[i] == i*d+mn)
+                i++;
+            else if((arr[i]-mn)%d != 0)
+                return false;
+            else {
+                int pos = (arr[i]-mn)/d;
+                if(pos < i)
+                    return false;
+                swap(arr[i], arr[pos]);
+            }
+        }
+        return true;
+    }
+};
+
+// OR O(nlogn) solution
+
+
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
         sort(arr.begin(), arr.end());
         int d = arr[1] - arr[0];
         for(int i = 2; i < arr.size(); i++) {
