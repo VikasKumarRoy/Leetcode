0aab59fda5691dbba160d613bbe5af7f7e6f3fbc
Vikas Roy <vikas.vr8@gmail.com>
2020-07-12 13:33:20 +0530

    Maximum Width of Binary Tree(Medium)

--- Problem-662.cpp
+++ Problem-662.cpp
@@ -2,22 +2,28 @@
 
 // https://leetcode.com/problems/maximum-width-of-binary-tree/
 
-// O(n) time complexity and O(n) space complexity solution
+// O(n) time complexity and O(n) space complexity solution using bfs 
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        
+        queue <pair <TreeNode*, int>> q;
+        q.push({root, 1});
+        int ans = 1;
+        while(!q.empty()) {
+            int sz = q.size();
+            int offset = q.front().second;
+            ans = max(ans, (q.back().second - offset + 1));
+            while(sz--) {
+                TreeNode* node = q.front().first;
+                int pos = q.front().second - offset;
+                q.pop();
+                if(node->left)
+                    q.push({node->left, 2*pos});
+                if(node->right)
+                    q.push({node->right, 2*pos+1});
+            }
+        }
+        return ans;
     }
 };
