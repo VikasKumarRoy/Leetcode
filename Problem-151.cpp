4c2ebbdf1551159bea00185096d9ee85eb34e46b
Vikas Roy <vikas.vr8@gmail.com>
2020-07-16 01:57:42 +0530

    Reverse Words in a String(Medium

---
+++ Problem-151.cpp
@@ -0,0 +1,19 @@
+// Problem - 151
+
+// https://leetcode.com/problems/reverse-words-in-a-string/
+
+// O(n) time complexity and O(n) space complexity solution
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        if (!s.length())
+            return "";
+        stringstream ss(s);
+        string word, ans;
+        while(ss >> word) {
+            ans = word + " " + ans;
+        }
+        return ans.substr(0,ans.length()-1);
+    }
+};
