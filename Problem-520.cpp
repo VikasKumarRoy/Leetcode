19494154ebb119b8a63c69eef04459107d06243e
Vikas Roy <vikas.vr8@gmail.com>
2020-08-03 23:57:04 +0530

    Detect Capital(Easy)

--- Problem-520.cpp
+++ Problem-520.cpp
@@ -3,3 +3,18 @@
 // https://leetcode.com/problems/detect-capital/
 
 // O(n) time complexity and O(1) space complexity solution
+
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        if(word.length() < 2)
+            return true;
+        for(int i = 0; i < word.size(); i++) {
+            if(isupper(word[i]) != isupper(word[1])) {
+                if((i == 0 && !isupper(word[i])) || i > 1)
+                    return false;
+            }
+        }
+        return true;
+    }
+};
