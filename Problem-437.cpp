b68e335722cab5e41a166508edc1474c9717281e
Vikas Roy <vikas.vr8@gmail.com>
2020-08-10 23:47:24 +0530

    Path Sum III(Medium)

--- Problem-437.cpp
+++ Problem-437.cpp
@@ -2,41 +2,27 @@
 
 // https://leetcode.com/problems/path-sum-iii/
 
-// O(n) time complexity and O(n) space complexity solution
+// O(n) time complexity and O(n) space complexity solution using dfs
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int ans = 0;
     
-    void fun(TreeNode* root, int sum, vector<int> path, int sumTillNow) {
-        
-        if(root==NULL) return;
-        
-        path.push_back(root->val);
-        
-        sumTillNow+=root->val;
-                
-        if(root->right==NULL && root->left==NULL && sumTillNow==sum)
-            ans.push_back(path);
-        
-        fun(root->left, sum, path, sumTillNow);
-        fun(root->right, sum, path, sumTillNow);
-        
+    void util(TreeNode* root, int sum, unordered_map <int, int> um, int currSum) {
+        if(!root)
+            return;
+        currSum += root->val;
+        if(um.count(currSum - sum))
+            ans += um[currSum - sum];
+        um[currSum]++;
+        util(root->left, sum, um, currSum);
+        util(root->right, sum, um, currSum);
     }
     
     int pathSum(TreeNode* root, int sum) {
-        fun(root, sum, );
+        unordered_map <int, int> um;
+        um[0]++;
+        util(root, sum, um, 0);
         return ans;
     }
 };
