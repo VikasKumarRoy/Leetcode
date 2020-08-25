1b1456291f52bbf953f3334fc3b21e8c2bee978e
Vikas Roy <vikas.vr8@gmail.com>
2020-08-25 23:58:18 +0530

    Create Problem-1560.cpp

---
+++ Problem-1560.cpp
@@ -0,0 +1,12 @@
+// Problem - 1560
+
+// https://leetcode.com/problems/most-visited-sector-in-a-circular-track/
+
+// O(n) time complexity and O(n) space complexity solution
+
+class Solution {
+public:
+    vector<int> mostVisited(int n, vector<int>& rounds) {
+        
+    }
+};
