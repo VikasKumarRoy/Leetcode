572505a8d5d75aa6b161623441782d6da38c1d1c
Vikas Roy <vikas.vr8@gmail.com>
2020-10-24 02:21:31 +0530

    132 Pattern(Medium)

--- Problem-456.cpp
+++ Problem-456.cpp
@@ -9,13 +9,49 @@ public:
     bool find132pattern(vector<int>& nums) {
         if(nums.size() < 3)
             return false;
-        int mn = min(nums[0], nums[1]);
-        int mx = max(nums[0], nums[1]);
+        stack <int> s;
+        int max2 = INT_MIN;
+        for(int i = nums.size()-1; i >= 0; i--) {
+            if(s.empty() || nums[i] > s.top()) {
+                while(!s.empty() && nums[i] > s.top()) {
+                    max2 = s.top();
+                    s.pop();
+                }
+            }
+            else {
+                if(nums[i] < max2)
+                    return true;
+            }
+            s.push(nums[i]);
+        }
+        return false;
+    }
+};
+
+// OR O(n^2) quadratic approach with constant space
+
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        if(nums.size() < 3)
+            return false;
         for(int i = 2; i < nums.size(); i++) {
-            if(nums[i] < mx && nums[i] > mn)
+            int mn, mx;
+            mn = mx = -1;
+            for(int j = 0; j < i; j++) {
+                if(nums[j] < nums[i]) {
+                    mn = j;
+                    break;
+                }
+            }
+            for(int j = i-1; j >= 0; j--) {
+                if(nums[j] > nums[i]) {
+                    mx = j;
+                    break;
+                }
+            }
+            if(mn != -1 && mx != -1 && mn < mx)
                 return true;
-            mn = min(mn, nums[i]);
-            mx = max(mx, nums[i]);
         }
         return false;
     }
