0b97a0212d2161c230bdf3f45a9dbc8e223e715a
Vikas Roy <vikas.vr8@gmail.com>
2020-10-25 18:33:38 +0530

    Stone Game IV(Hard)

---
+++ Problem-1510.cpp
@@ -0,0 +1,21 @@
+// Problem - 1510
+
+// https://leetcode.com/problems/stone-game-iv/
+
+// O(n*sqrt(n)) time complexity and O(n) space complexity solution using dp
+
+class Solution {
+public:
+    bool winnerSquareGame(int n) {
+        vector <bool> dp(n+1);
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j * j <= i; j++) {
+                if(dp[i-j*j] == false) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
