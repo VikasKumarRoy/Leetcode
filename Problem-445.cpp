41c832437db68182f19910350583badef06a0653
Vikas Roy <vikas.vr8@gmail.com>
2020-11-09 01:53:06 +0530

    Add Two Numbers II(Medium)

--- Problem-445.cpp
+++ Problem-445.cpp
@@ -38,3 +38,57 @@ public:
         return prev;
     }
 };
+
+// OR O(n) time complexity and O(1) space complexity solution
+
+class Solution {
+public:
+    int getLength(ListNode* l) {
+        int ctr = 0;
+        while(l) {
+            ctr++;
+            l = l->next;
+        }
+        return ctr;
+    }
+    
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* prev = NULL;
+        int n1 = getLength(l1), n2 = getLength(l2);
+        if(n2 > n1)
+            swap(l1, l2);
+        int ctr = abs(n1 - n2);
+        while(ctr--) {
+            ListNode* node = new ListNode(l1->val);
+            l1 = l1->next;
+            node->next = prev;
+            prev = node;
+        }
+        while(l1 && l2) {
+            ListNode* node = new ListNode(l1->val + l2->val);
+            l1 = l1->next;
+            l2 = l2->next;
+            node->next = prev;
+            prev = node;
+        }
+        int carry = 0;
+        // Normalization and reverse the ans list
+        ListNode* head = prev;
+        prev = NULL;
+        while(head) {
+            head->val += carry;
+            carry = head->val/10;
+            head->val %= 10;
+            ListNode* temp = head->next;
+            head->next = prev;
+            prev = head;
+            head = temp;
+        }
+        if(carry) {
+            ListNode* node = new ListNode(carry);
+            node->next = prev;
+            prev = node;
+        }
+        return prev;
+    }
+};
