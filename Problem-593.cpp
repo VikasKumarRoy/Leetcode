2164cbfdaaabe50a00540a8052e848aa85ef12d8
Vikas Roy <vikas.vr8@gmail.com>
2020-11-12 02:31:53 +0530

    Valid Square(Medium)

--- Problem-593.cpp
+++ Problem-593.cpp
@@ -2,43 +2,28 @@
 
 // https://leetcode.com/problems/valid-square/
 
-// O(1) time complexity and O(1) space complexity solution
+// O(1) time complexity and O(1) space complexity solution by comparing 4 sides and diagnol lenght
 
 class Solution {
 public:
     const float PI = 3.141592;
     
-    float distance(vector <int>& p1, vector <int>& p2) { 
-        return sqrt(pow(p1[0] - p2[0], 2) +  pow(p1[1] - p2[1], 2) * 1.0); 
-    }
-    
-    float getAngle(vector <int>& p1, vector <int>& p2) {
-        if(abs(p1[0] - p2[0]) == 0)
-            return 90;
-        float slope = (float)(p1[1] - p2[1]) / (p1[0] - p2[0]);
-        float A = float(atan(slope)) * 180 / PI;
-        cout << A << endl;
-        return A;
+    int distance(vector <int>& p1, vector <int>& p2) { 
+        return (pow(p1[0] - p2[0], 2) +  pow(p1[1] - p2[1], 2)); 
     }
     
     bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
-        float l1 = distance(p1, p2);
-        float l2 = distance(p1, p3);
-        float l3 = distance(p1, p4);
-        if(l2 > l1 && l2 > l3) {
-            swap(p2, p3);
-        }
-        if(l3 > l1 && l3 > l2) {
-            swap(p2, p4);
-        }
-        l1 = distance(p1, p2);
-        l2 = distance(p3, p4);
-        if(l1 != l2)
+        vector <int> v(6);
+        v[0] = distance(p1, p2);
+        v[1] = distance(p1, p3);
+        v[2] = distance(p1, p4);
+        v[3] = distance(p2, p3);
+        v[4] = distance(p2, p4);
+        v[5] = distance(p3, p4);
+        sort(v.begin(), v.end());
+        if(v[0] == 0 || v[1] != v[0] || v[0] != v[2] || v[0] != v[3])
             return false;
-        float angle =  abs(getAngle(p1, p2) - getAngle(p3, p4));
-        float t = 0.01;
-        cout << "angle " << angle <<endl;
-        if(angle > 90+t || angle < 90-t)
+        if(v[4] != v[5])
             return false;
         return true;
     }
