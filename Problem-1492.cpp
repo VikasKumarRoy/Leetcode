ced3a878891cdedfe0413180bd98cf3120ea6da3
Vikas Roy <vikas.vr8@gmail.com>
2020-12-04 23:59:14 +0530

    The kth Factor of n (Medium)

---
+++ Problem-1492.cpp
@@ -0,0 +1,25 @@
+// Problem - 1492
+
+// https://leetcode.com/problems/the-kth-factor-of-n/
+
+// O(sqrt(n)) time complexity and O(1) space complexity solution
+
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        int c = 1;
+        for(int i = 1; i < sqrt(n); i++) {
+            if(n%i)
+                k--;
+            if(k == 0)
+                return i;
+        }
+        for(int i = sqrt(n); i >= 1; i--) {
+            if(n%i)
+                k--;
+            if(k == 0)
+                return n/i;
+        }
+        return -1;
+    }
+};
