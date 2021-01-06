9f93403538d58020d428fa03bf9d0ea116e8fe0a
Vikas Roy <vikas.vr8@gmail.com>
2021-01-06 23:58:35 +0530

    Kth Missing Positive Number(Easy)

---
+++ Problem-1539.cpp
@@ -0,0 +1,28 @@
+// Problem - 1539
+
+// https://leetcode.com/problems/kth-missing-positive-number/
+
+// O(n) time complexity and O(1) space complexity solution
+
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int next = 1;
+        int n = arr.size();
+        int i = 0;
+        while(i < n) {
+            if(arr[i] == next) {
+                next++;
+            }
+            else {
+                if(arr[i] - next >= k)
+                    return next+k-1;
+                else
+                    k -= arr[i]-next;
+                next = arr[i]+1;
+            }
+            i++;
+        }
+        return arr[n-1]+k;
+    }
+};
