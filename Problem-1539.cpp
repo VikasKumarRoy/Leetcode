b0437aa4da021218c01edb11622beb55baa5a385
Vikas Roy <vikas.vr8@gmail.com>
2021-01-07 22:51:23 +0530

    Kth Missing Positive Number(Easy)

--- Problem-1539.cpp
+++ Problem-1539.cpp
@@ -26,3 +26,28 @@ public:
         return arr[n-1]+k;
     }
 };
+
+// OR O(logn) time using binary search
+
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int next = 1;
+        int n = arr.size();
+        int i = 0;
+        while(i < n) {
+            if(arr[i] == next) {
+                next++;
+            }
+            else {
+                if(arr[i] - next >= k)
+                    return next+k-1;
+                else
+                    k -= arr[i]-next;
+                next = arr[i]+1;
+            }
+            i++;
+        }
+        return arr[n-1]+k;
+    }
+};
