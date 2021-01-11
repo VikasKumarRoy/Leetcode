22712686b5ac26047fc9c9fb8bb0bd7c08437b8b
Vikas Roy <vikas.vr8@gmail.com>
2021-01-12 00:07:34 +0530

    Decode Ways(Medium)

--- Problem-91.cpp
+++ Problem-91.cpp
@@ -30,3 +30,25 @@ public:
         return recurr(s, 0, mem);
     }
 };
+
+// OR O(n) time complexity and O(n) space complexity solution using bottom approach
+
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.length();
+        vector <int> dp(n+1, 0);
+        dp[0] = 1;
+        dp[1] = s[0] == '0' ? 0 : 1;
+        for(int i = 2; i <= n; i++) {
+            int f1 = stoi(s.substr(i-2, 2));
+            int f2 = s[i-1]-'0';
+            if(f2 >= 1)
+                dp[i] += dp[i-1];
+            if(f1 >= 10 && f1 <= 26) {
+                dp[i] += (i > 1 ? dp[i-2] : 0);
+            }
+        }
+        return dp[n];
+    }
+};
