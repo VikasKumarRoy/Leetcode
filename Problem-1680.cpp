516bebd49a0a8c3960acec6fe06a0926b4daa8ad
Vikas Roy <vikas.vr8@gmail.com>
2021-01-27 23:55:38 +0530

    Concatenation of Consecutive Binary Numbers(Medium)

---
+++ Problem-1680.cpp
@@ -0,0 +1,12 @@
+// Problem - 1680
+
+// https://leetcode.com/problems/concatenation-of-consecutive-binary-numbers/
+
+// O(n) time complexity and O(1) space complexity solution
+
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        
+    }
+};
